3e82f9a7b412957aab593232d4bf4ce4efee793e
Miguel Hojman <miguelhojman@gmail.com>
2023-06-04 20:40:46 -0300

    primer commit creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    NavbarComponent,
+    ListadoNoticiasComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
